8cff453d7b4ac1bc79ca29df79ea63ddc6664903
Marlyn12345 <161170197+Marlyn12345@users.noreply.github.com>
2024-09-28 12:35:13 -0600

    Add files via upload

---
+++ Segundo programa.py
@@ -0,0 +1,15 @@
+def cuadrado():
+    numero = int(input("Ingresa un número entero: "))
+    resultado = numero ** 2
+    print(f"El cuadrado de {numero} es {resultado}")
+
+def producto():
+    num1 = int(input("Ingresa el primer número entero: "))
+    num2 = int(input("Ingresa el segundo número entero: "))
+    resultado = num1 * num2
+    print(f"El producto de {num1} y {num2} es {resultado}")
+
+
+cuadrado()
+producto()
+ 
